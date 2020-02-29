a565a7980d3ebc678f5b77899e335982fe3df8a6
imoremada <imoremada@gmail.com>
2020-03-01 05:48:54 +0800

    Profiles have been added, Updated the pom.xml

--- src/main/java/com/reverston/rms/RestaurantApplication.java
+++ src/main/java/com/reverston/rms/RestaurantApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurentApplication {
+public class RestaurantApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurentApplication.class, args);
+		SpringApplication.run(RestaurantApplication.class, args);
 	}
 
 }
