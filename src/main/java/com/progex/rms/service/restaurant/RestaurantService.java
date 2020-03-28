7c97634599ebc11414e0c765c33a27c6822513b1
imoremada <imoremada@gmail.com>
2020-03-28 18:16:00 +0800

    RestaurantService has been added

---
+++ src/main/java/com/progex/rms/service/restaurant/RestaurantService.java
@@ -0,0 +1,7 @@
+package com.progex.rms.service.restaurant;
+
+import com.progex.rms.resource.restaurant.Restaurant;
+
+public interface RestaurantService {
+    Restaurant add(Restaurant restaurant);
+}
